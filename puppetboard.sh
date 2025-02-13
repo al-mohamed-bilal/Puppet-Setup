acbc1a6b9c2390d939cdc8aaaa176243e28606c1
Al Mohamed Bilal <95761008+al-mohamed-bilal@users.noreply.github.com>
2025-02-13 10:51:20 +0530

    Uploaded PuppetBoard Script

---
+++ puppetboard.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Declaring the directory of  Virtual Environment
+venv_dir="puppetboard-venv"
+
+echo "Creating Virtual Environment..."
+
+# Installing python3 and other dependencies to install Puppetboard
+if ! command -v python3 1> /dev/null
+then
+    apt install python3 python3-dev libpq-dev python3-pip python3-venv -y 1> /dev/null 2> /dev/null
+    echo "Installed Python successfully"
+else
+    echo "Python is already installed"
+fi
+
+# Creating Virtual Environment to install Puppetboard
+if [ ! -d $venv_dir ]
+then
+    python3 -m venv $venv_dir
+    echo "Successfully created the virtual environment $venv_dir"
+else
+    echo "$venv_dir Virtual Environment already exists"
+fi
+
+# Activating the Virtual Environment
+source "$venv_dir/bin/activate"
+echo "Sucessfully activated Virtual Environment"
+
+# Installing Puppetboard package
+pip3 install puppetboard 1> /dev/null 2> /dev/null
+echo "Successfully installed Puppetboard"
+
+# Moving to the application directory
+cd "$venv_dir/lib/python3.11/site-packages/puppetboard"
+
+# Changing PuppetDB hostname in default_settings.py
+echo "Enter hostname of PuppetDB"
+read hostname
+sed -i "/PUPPETDB_HOST/c\PUPPETDB_HOST = '$hostname'" default_settings.py
+echo "Successfully changed PuppetDB hostname in  default_settings.py"
+
+# Generating the secret key for running of Puppetboard
+key=$(python3 -c "import secrets;print(secrets.token_hex(16))")
+sed -i "/SECRET_KEY/c\SECRET_KEY = '$key'" default_settings.py
+echo "Successfully added the secret key to default_settings.py"
+
+# Running Puppetboard 
+echo "Running Puppetboard..."
+echo "Copy paste the below URL in your web browser"
+echo "http://`hostname`:5000"
+flask run --host=0.0.0.0 1> /dev/null 2> /dev/null
